eb048e2ff950e6a51be807e23be3a18ccbde82f5
Sloth-iNkt <ciavelm18@gmail.com>
2025-05-23 00:06:32 +0800

    chore: remove framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import { Home, About, Projects, Contact } from './pages';
 import Navbar from './components/Navbar';
-import { AnimatePresence } from 'framer-motion';
-
-const AnimatedRoutes = () => {
-  const location = useLocation();
-
-  return (
-     <div className="relative">
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </AnimatePresence>
-    </div>
-  );
-};
 
 const App = () => {
+
   return (
     <main className="bg-[#202221] text-white">
         <Router>
-            <Navbar className="fixed top-0 left-0 w-full z-50" />
-            <AnimatedRoutes />
+            <Navbar className="fixed top-0 left-0 w-full z-50"/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         </Router>
     </main>
   )
